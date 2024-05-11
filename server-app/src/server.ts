42a4070f5efafd20b8633d1d412aceda6a39bad3
sumit kumar <sumit1523@gmail.com>
2024-05-11 14:52:38 +0530

    mongodb setup and middleware cors, json urlencoded

--- server-app/src/server.ts
+++ server-app/src/server.ts
@@ -1,16 +1,28 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import connectDb from "./config/dbConfig";
+
 dotenv.config();
+connectDb();
 
 const port = process.env.PORT || 5001;
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
 
 app.get("/", (req, res) => {
   res.send("Hello World");
   console.log("Hello World");
 });
+
 app.listen(port, () => {
   console.log(`Server Started Sucessfully : ${port}`);
 });
