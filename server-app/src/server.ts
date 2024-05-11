5ecca01035220cecf0feb568d6c9e7a3c6a9172c
sumit kumar <sumit1523@gmail.com>
2024-05-11 14:26:13 +0530

    typescript setup for node js

---
+++ server-app/src/server.ts
@@ -0,0 +1,16 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+dotenv.config();
+
+const port = process.env.PORT || 5001;
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+  console.log("Hello World");
+});
+app.listen(port, () => {
+  console.log(`Server Started Sucessfully : ${port}`);
+});
